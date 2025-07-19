5ab52a5464acb0e00f87c38505d028b69b09ccce
jhg <jhg0675@github.com>
2025-07-20 02:26:21 +0900

    RepositionTile 수정

--- Assets/Scripts/Battle/RepositionTile.cs
+++ Assets/Scripts/Battle/RepositionTile.cs
@@ -10,30 +10,33 @@ public class RepositionTile : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (!collision.gameObject.CheckLayer(checkLayer))
-            return;
-
-        if (!BattleSceneManager.Instance)
+        if (!collision.gameObject.CheckLayer(checkLayer) || !BattleSceneManager.Instance)
             return;
 
         Vector3 playerPos = BattleSceneManager.Instance.CurrentCharacter.transform.position;
-        Vector3 pos = transform.position;
+        Vector3 tilePos = transform.position;
 
-        float diffX = Mathf.Abs(playerPos.x - pos.x);
-        float diffY = Mathf.Abs(playerPos.y - pos.y);
+        float diffX = playerPos.x - tilePos.x;
+        float diffY = playerPos.y - tilePos.y;
 
-        Vector3 playerDir = BattleSceneManager.Instance.CurrentCharacter.Model.InputWrapper.Movement;
-        float dirX = playerDir.x < 0 ? -1 : 1;
-        float dirY = playerDir.y < 0 ? -1 : 1;
+        // 절대값으로 주 이동 방향 판단
+        float absDiffX = Mathf.Abs(diffX);
+        float absDiffY = Mathf.Abs(diffY);
 
-        if (diffX > diffY)
+        Vector3 offset = Vector3.zero;
+
+        if (absDiffX > absDiffY) // 수평 이동이 더 큼
         {
-            transform.Translate(dirX * totalChunkSize * Vector3.right);
+            // diffX가 양수면 플레이어가 타일의 오른쪽에 있다는 의미 -> 타일을 오른쪽으로 옮겨야 함
+            // diffX가 음수면 플레이어가 타일의 왼쪽에 있다는 의미 -> 타일을 왼쪽으로 옮겨야 함
+            offset.x = (diffX > 0 ? 1 : -1) * totalChunkSize;
         }
-        else
+        else // 수직 이동이 더 큼
         {
-            transform.Translate(dirY * totalChunkSize * Vector3.up);
+            offset.y = (diffY > 0 ? 1 : -1) * totalChunkSize;
         }
+
+        transform.position += offset;
 
         if (BattleSceneManager.Instance)
             BattleSceneManager.Instance.RecalculatePath();
