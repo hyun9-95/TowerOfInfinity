3ffec74b4032815c7741a98577377ac0375f4639
jhg <jhg0675@github.com>
2025-07-26 19:40:23 +0900

    불필요 로그 제거

--- Assets/Scripts/Common/AddressableMono.cs
+++ Assets/Scripts/Common/AddressableMono.cs
@@ -4,8 +4,6 @@ public class AddressableMono : MonoBehaviour
 {
     public virtual void OnDestroy()
     {
-        Logger.Log($"{gameObject.name} destroyed.");
-
         if (gameObject != null)
             AddressableManager.Instance.ReleaseGameObject(gameObject);
     }
