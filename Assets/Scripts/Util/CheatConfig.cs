089550fac9cd277fe2949cb962791d19b95243cf
jhg <jhg0675@github.com>
2025-08-16 18:06:50 +0900

    CheatConfig 헤더 안뜨는거 제거

--- Assets/Scripts/Util/CheatConfig.cs
+++ Assets/Scripts/Util/CheatConfig.cs
@@ -3,22 +3,16 @@ using UnityEngine;
 [System.Serializable]
 public class CheatConfig 
 {
-    [Header("전투로 바로 진입")]
     public bool IsEnterBattleDirectly;
 
-    [Header("전투에서 추가할 능력")]
     public AbilityDefine[] cheatAbility;
 
-    [Header("커스터마이징 플로우 진입")]
     public bool IsEnterCustomizationFlow;
 
-    [Header("Walkable Node + 경로 표시")]
     public bool IsDebugAStar;
 
-    [Header("무적")]
     public bool IsInvincible;
 
-    [Header("시작 마을 씬")]
     public SceneDefine SceneDefine;
 
     public bool enableGcAllocCallStack = false;
