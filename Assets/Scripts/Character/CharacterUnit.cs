42c8cd6769c977ed54da39963f2bafe0e9401bb6
jhg <jhg0675@github.com>
2025-08-18 23:45:35 +0900

    조건체크 잘못된거 수정

--- Assets/Scripts/Character/CharacterUnit.cs
+++ Assets/Scripts/Character/CharacterUnit.cs
@@ -95,7 +95,7 @@ public class CharacterUnit : PoolableMono
         PrepareState();
         ProcessCurrentState();
 
-        if (!CheckUpdatableState())
+        if (CheckUpdatableState())
             CheckNextState();
     }
 
