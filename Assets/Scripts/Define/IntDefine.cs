d60eee013a33c00667056d6e6a017b3cfa8e9953
jhg <jhg0675@github.com>
2025-08-23 02:52:28 +0900

    어빌리티, 배틀이벤트 최대레벨 5로

--- Assets/Scripts/Define/IntDefine.cs
+++ Assets/Scripts/Define/IntDefine.cs
@@ -2,7 +2,7 @@ public class IntDefine
 {
     public const int DEFAULT_CHARACTER_PLAYER_ID = 10000;
     public const int MILLI_SECOND = 1000;
-    public const int MAX_ABILITY_LEVEL = 8;
+    public const int MAX_ABILITY_LEVEL = 5;
     public const int MAX_BATTLE_LEVEL = 30;
     public const int MAX_DUNGEON_WAVE_COUNT = 15;
     public const int MID_BOSS_WAVE = 7;
@@ -10,4 +10,5 @@ public class IntDefine
     public const int BATTLE_CARD_DRAW_COUNT = 3;
     public const int HIT_BLINK_COUNT = 2;
     public const int STATE_DEAD_PRIORITY = 100;
+    public const int MAX_ABILITY_SLOT_COUNT = 5;
 }
